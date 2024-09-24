a9389464bfa1bac6fe52b1d728b1331c8d73046e
Lbayan <bayan.ltaief@thekaustschool.org>
2024-09-24 12:32:24 +0300

    Add files via upload

---
+++ function calculate().jl
@@ -0,0 +1,32 @@
+#Create a function 
+function calculate()
+    #Input: recieve number and operation from user
+    println("Enter a number: ")
+    num1 = parse(Float64, readline())   
+    println("Enter a operation(+,-,*,/): ")
+    operation = readline()
+    println("Enter another number: ")
+    num2 = parse(Float64, readline())
+    #Conditionals: based on input -> result
+    if operation == "+"
+        result = num1 + num2
+        println("The result is: ", result)
+    elseif operation == "-"
+        result = num1 - num2
+        println("The result is: ", result)
+    elseif operation == "*"
+        result = num1 * num2
+        println("The result is: ", result)
+    elseif operation == "/"
+        if num2 != 0    
+            result = num1 / num2
+            println("The result is: ", result)
+        else 
+            return println("Error, Invalid Number") #If number 2  = 0, you cant divide 
+        end
+    else 
+        return println("Error, Invlaid Operation")  #If user put another operation -> returns invalid 
+    end
+end
+
+calculate() # calls the function 
